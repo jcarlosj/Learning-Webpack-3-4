c18cdf05cf1cf006298e9a40753f7c4d2e66d6b8
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-07-19 13:02:44 -0500

    Refactoriza modo desarrollador para Webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // Crea tarea:
 module .exports = {
+    mode: 'development',                         /* Asigna el modo de transpilación */
     entry: './index.js',                         /* Asigna el 'entry point' */
     output: {                                    /* Asigna el 'output' */
         filename: 'bundle.js'
